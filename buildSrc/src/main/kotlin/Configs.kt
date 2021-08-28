8ec5dbde9abed1046b1c478a4f6c758a2f4d7db9
Alvin Dizon <alvin.dizon91@gmail.com>
2021-08-28 14:08:09 +0800

    Bump min and compile/target SDKs to 23 and 31 respectively
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -1,8 +1,8 @@
 object Configs {
     const val applicationId = "com.alvindizon.tampisaw"
-    const val compileSdkVersion = 30
-    const val minSdkVersion = 21
-    const val targetSdkVersion = 30
+    const val compileSdkVersion = 31
+    const val minSdkVersion = 23
+    const val targetSdkVersion = 31
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val versionCode = 1
     const val versionName = "1.0.0"
